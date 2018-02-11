d555b204f261b492eae002e82ae136d876787532
a-bad-programmer <quintinf19@parkschool.net>
2018-02-11 14:19:40 -0500

    Added basic funtionality for joystick, not really working but whatever. Also realised that "verify" was a thing in arduino.

--- OperatorController/OperatorController.ino
+++ OperatorController/OperatorController.ino
@@ -4,40 +4,54 @@
 
 CapacitiveSensor touch = CapacitiveSensor(0, 1);
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
-  1, 0
+  1, 0,
   false, false, true,
   false, false, false,
   false, false,
   false, false, false);
 
-const int button1 = 2;
-const int button2 = 3;
-const int button3 = 4;
-const int button4 = 5;
-const int button5 = 6;
-const int button6 = 7;
+const int potPin = 2;
+int potVal = 0;
+
+const int button1 = 1;
+const int button2 = 2;
+const int button3 = 3;
+const int button4 = 4;
+const int button5 = 5;
+const int button6 = 6;
+
+const int led1 = 7;
+const int led2 = 8;
+const int led3 = 9;
+const int led4 = 10;
+const int led5 = 11;
+const int led6 = 12;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(button1, INPUT_PULLUP);
-
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
+  pinMode(button4, INPUT_PULLUP);
+  pinMode(button5, INPUT_PULLUP);
+  pinMode(button6, INPUT_PULLUP);
+  
+  pinMode(led1, OUTPUT);
   Joystick.begin();
-  Joystick.setZAxisRange(-1, 1);
 }
 
 int lastButtonState[6] = {0,0,0,0,0,0};
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (index = 0; index < 6; index ++) {
-    int currentButtonState !=digitalRead(index + button1);
-    if(currentButtonState != lastButtonState[index]);
-    switch (index){
-    case 0:
-      if(currentButtonState == 1){
+  for (int index = 0; index < 6; index ++) {
+    int currentButtonState = !digitalRead(index + button1);
+    if(currentButtonState != lastButtonState[index]){
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+  }
+  }
+  Joystick.setZAxis(analogRead(potPin));
+  delay(50);
+  }
 
-      }
-    }
-  }
-  }
-  
-}
