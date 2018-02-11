7aab1a0b747b0cfc9767d52550e3c66968e10dfd
a-bad-programmer <quintinf19@parkschool.net>
2018-02-11 13:19:46 -0500

    Started tracking arduino files

---
+++ OperatorController/OperatorController.ino
@@ -0,0 +1,15 @@
+#include <CapacitiveSensor.h>
+
+#include <Joystick.h>
+
+CapacitiveSensor touch = CapacitiveSensor(0, 1);
+void setup() {
+  // put your setup code here, to run once:
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
