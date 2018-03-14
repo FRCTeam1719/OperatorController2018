d509dab020ac3a67ceafd398d06e205a9936ea72
a-bad-programmer <quintinf19@parkschool.net>
2018-03-14 15:33:53 -0400

    make the hacked python NetworkTables method work(?), untested but hopefully will work good.

--- DriverStationNetworkTables/NetworkToSerial.py
+++ DriverStationNetworkTables/NetworkToSerial.py
@@ -1,18 +1,39 @@
 from networktables import NetworkTables
+import warnings
 import serial
+import serial.tools.list_ports
 from time import sleep
 
 
 '''Setup the network tables and serial'''
-NetworkTables.initialize(server='roborio-1719-frc.local') #Test this later
+NetworkTables.initialize(server='10.17.19.2') #Test this later
+print('Searching for Arduino, please wait and make sure the device is plugged in...')
+while 1:
+    arduino_ports = [
+        p.device
+        for p in serial.tools.list_ports.comports()
+        if 'Arduino' in p.description
+    ]
+    if arduino_ports:
+        break
+
+if len(arduino_ports) > 1:
+    warnings.warn('Multiple Arduinos found - using the first')
+else:
+    print('Arduino found.')
+ser = serial.Serial(arduino_ports[0], 9600)
+
 ser = serial.Serial('COM1', 9600) #The UsbSerial NEEDS to be set to the proper port name.
 
 table = NetworkTables.getTable('SmartDashboard') #The getXxxx can be whatever aslong as it is a SmartDashboard thing (i.e getData or getBoolean)
 
 while NetworkTables.isConnected():
-    height = table.getNumber('ELEVATOR_HEIGHT', -80)
+    height = int(table.getNumber('ELEVATOR_HEIGHT', -80))
 
     #height_encode = b'%d' %height # byte 'double' %regex in height
-    height_encode = int(height).to_bytes(2, byteorder='little', signed=True)
-    ser.write(ser.write(str(height).encode()))
+    height_encode = b'%d' %height  # you need to change %d based on the type your variable
+    ser.write(height_encode)
+    if(ser.in_waiting >0):
+        line = ser.readline()
+        print(line)
     sleep(.01) # Serial delay to make things work nice
